826efd63dedd226e7588e7fd576fbeabeb92ae28
ivannikova28 <128988399+ivannikova28@users.noreply.github.com>
2024-05-14 10:48:23 +0300

    fix

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -3,7 +3,6 @@ import iziToast from "izitoast";
 // Додатковий імпорт стилів
 import "izitoast/dist/css/iziToast.min.css";
 
-
 const form = document.querySelector('.form');
 
 form.addEventListener('submit', function (event) {
